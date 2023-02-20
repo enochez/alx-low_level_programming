008b129ea3a3ef1436094069ebf904bd2cb7a9a6
enochez <enockmutai21118@gmail.com>
2023-02-20 10:49:04 -0800

    print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Prints numbers from 0 to 9.
+*
+* Return: return Always 0 (Success)
+*/
+int main(void)
+{
+	int a=0;
+	while (a<=9)
+	{
+		printf("%d",a);
+		a++;
+	}
+	putchar('\n');
+	return 0;
+}
+
