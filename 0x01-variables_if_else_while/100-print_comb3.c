88f0208148c0a5e4892469ede20a642a17363b01
enochez <enockmutai21118@gmail.com>
2023-02-20 11:19:26 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: return value (0)
+*/
+int main(void)
+{
+int i, j;
+i = 48;
+j = 48;
+while (j < 58)
+{
+i = 48;
+while (i < 58)
+{
+if (j != i && j < i)
+{
+putchar(j);
+putchar(i);
+if (i == 57 && j == 56)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+j++;
+}
+putchar('\n');
+
+return (0);
+}
