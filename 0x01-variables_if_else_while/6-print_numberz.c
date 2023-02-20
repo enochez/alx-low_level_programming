f386c0260c013b5efa7f514d03cfca07e071a344
enochez <enockmutai21118@gmail.com>
2023-02-20 11:00:16 -0800

    print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints numbers between 0 to 9.
+*
+* Return: return Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
