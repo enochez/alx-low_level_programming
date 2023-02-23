7a7a3bc2a65c69513864aef911e6fbd676e1a9b5
enochez <enockmutai21118@gmail.com>
2023-02-23 14:00:30 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers from 0 - 9.
+ *
+ *Return: no return value.
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
