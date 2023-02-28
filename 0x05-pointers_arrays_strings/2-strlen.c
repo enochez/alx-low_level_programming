a787df6fa8d6670e1d9818a76d5ccc3d0d0513fd
enochez <enockmutai21118@gmail.com>
2023-02-28 15:24:40 -0800

    strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+
+/**
+ *_strlen - returns the length of a string
+ *
+ *@s: a string
+ *
+ *Return: returns the size of the string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	i = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+
+}
