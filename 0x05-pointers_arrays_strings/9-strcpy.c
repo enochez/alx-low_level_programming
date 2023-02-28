0a689cf69b8f53b6b56b073243300889ffc8364f
enochez <enockmutai21118@gmail.com>
2023-02-28 15:52:22 -0800

    strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ **_strcpy - copies the string to be printed to src
+ *
+ *@dest: pointer to buffer
+ *
+ *@src: string to be copied
+ *
+ *Return: returns a pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
