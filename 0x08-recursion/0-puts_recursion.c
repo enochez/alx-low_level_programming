22d9beab9262e663aceb4d7a6ca14b8e49a2c8eb
enochez <enockmutai21118@gmail.com>
2023-03-08 14:54:52 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_string - prints string followed by new line
+ * @str: the string to be printed
+ * 
+ * Return void
+ */
+
+void print_string(char *str)
+{
+	if (str != NULL)
+	{
+		printf("%s\n", str);
+	}
+}
+
